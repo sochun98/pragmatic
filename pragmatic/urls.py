f6cbaaa088ef6bd4c46401399796379250c5748c
sochun98 <sochun98@naver.com>
2024-09-03 20:27:11 +0900

    20240903_profile_update

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 # from accountapp import urls as accountapp_urls
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
